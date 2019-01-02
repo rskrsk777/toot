60e91e257a11d9085f6c63aecbc1e98388ddc9ef
rskrsk777 <ryoosk777@outlook.jp>
2019-01-03 06:17:08 +0900

    Initial Commit

---
+++ toot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toot
+//
+//  Created by ryosuke kubo on 2019/01/03.
+//  Copyright © 2019 ryosuke kubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
